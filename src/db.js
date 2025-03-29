6ce06bc683e0cbdb02f5c56fa7b8def5567a1ee3
“Juan <ramirezjuanj0117@gmail.com>
2025-03-29 12:24:54 -0500

    Add .env for datbase

--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
 import pkg from "pg";
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./config.js"; 
+
 const { Pool } = pkg;
 
+
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "konectadb",
-  password: "0000",
-  port: 5432, 
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_NAME,
+  password: DB_PASSWORD,
+  port: DB_PORT, 
 });
